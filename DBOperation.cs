4f7e830d9df300dd189c81842d3aae0f7a9acc31
BrainholeRaccoon <184273520@qq.com>
2017-03-19 20:30:03 -0700

    iDog webservice

---
+++ DBOperation.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Data;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Xml.Linq;
+using System.Data.SqlClient;
+using System.Text.RegularExpressions;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace StockManageWebservice
+{
+    public class DBOperation:IDisposable
+    {
+        public static SqlConnection sqlCon; 
+     
+        private String ConServerStr = "Data Source=YINCHUAN-Y500;Initial Catalog=acc;Persist Security Info=True;User ID=sa;Password=123456";
+     
+        public DBOperation()
+        {
+            if (sqlCon == null)
+            {
+                sqlCon = new SqlConnection();
+                sqlCon.ConnectionString = ConServerStr;
+                sqlCon.Open();
+            }
+        }
+
+        public void Dispose()
+        {
+            if (sqlCon != null)
+            {
+                sqlCon.Close();
+                sqlCon = null;
+            }
+        }
+  
+        //
+        public List<string> selectAllUserInfor()
+        {
+            List<string> list = new List<string>();
+            try
+            {
+                
+                string sql = "select * from zsf";
+                SqlCommand cmd = new SqlCommand(sql,sqlCon);
+                SqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    list.Add(reader[0].ToString());
+                    list.Add(reader[1].ToString());
+                    list.Add(reader[2].ToString());
+
+                }
+                reader.Close();
+                cmd.Dispose();
+            }
+            catch(Exception)
+            {
+                     
+            }
+            return list;
+        }
+
+        public List<string> selectAllHealthInfor()
+        {
+            List<string> list = new List<string>();
+            try
+            {
+
+                string sql = "select * from zsfdog";
+                SqlCommand cmd = new SqlCommand(sql, sqlCon);
+                SqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    list.Add(reader[0].ToString());
+                    list.Add(reader[1].ToString());
+                    list.Add(reader[2].ToString());
+
+                }
+                reader.Close();
+                cmd.Dispose();
+            }
+            catch (Exception)
+            {
+
+            }
+            return list;
+        }
+        //
+        public bool insertUserInfo(string time,string food, string no)
+        {
+            try
+            {
+                
+                string sql = "insert into zsf (time,food,no) values ('" + time + "','" + food + "','" + no + "')";
+                SqlCommand cmd = new SqlCommand(sql, sqlCon);
+                cmd.ExecuteNonQuery();
+                cmd.Dispose();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        //
+        public bool deleteUserInfo(string no)
+        {
+            try
+            {
+                
+                string sql = "delete from zsf where no=" + no;
+                SqlCommand cmd = new SqlCommand(sql, sqlCon);
+                cmd.ExecuteNonQuery();
+                cmd.Dispose();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+        //
+        public bool updateUserMode(string no)
+        {
+            try
+            {
+
+                string sql = "update zsfmode set mode ="+no;
+                SqlCommand cmd = new SqlCommand(sql, sqlCon);
+                cmd.ExecuteNonQuery();
+                cmd.Dispose();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Service1.asmx.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace StockManageWebservice
+{
+    /// <summary>
+    /// Service1 的摘要说明
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // 若要允许使用 ASP.NET AJAX 从脚本中调用此 Web 服务，请取消对下行的注释。
+    //[System.Web.Script.Services.ScriptService]
+    public class Service1 : System.Web.Services.WebService
+    {
+        DBOperation dbOperation = new DBOperation();
+
+        
+        [WebMethod(Description = "select all user info")]
+        public string[] selectAllUserInfor()
+        {
+            return dbOperation.selectAllUserInfor().ToArray();
+        }
+
+        [WebMethod(Description = "select all health info")]
+        public string[] selectAllHealthInfor()
+        {
+            return dbOperation.selectAllHealthInfor().ToArray();
+        }
+
+
+        [WebMethod(Description = "add")]
+        public bool insertUserInfo(string time,string food, string no)
+        {
+            return dbOperation.insertUserInfo(time,food,no);
+        }
+
+        [WebMethod(Description = "delete")]
+        public bool deleteUserInfo(string no)
+        {
+            return dbOperation.deleteUserInfo(no);
+        }
+        [WebMethod(Description = "update mode")]
+        public bool updateUserMode(string no)
+        {
+            return dbOperation.updateUserMode(no);
+        }
+    }
+}
